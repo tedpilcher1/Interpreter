6e6b1d91d3aa0646d137b2b0eb7e6133b3509c9f
Ted Pilcher <ted.pilcher@glide.co.uk>
2024-07-28 18:38:36 +0100

    Created initial file reader and cmd line arg reader

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+mod error_handling;
+
+use std::env;
+use std::fs;
+use std::process;
+
+fn run(source: &str) -> Result<(), String> {
+    //  get tokens
+    // just print for now
+
+    return Err("Not implemented".to_string());
+}
+
+fn run_file(path: &str) -> Result<(), String> {
+    // let bytes: Vec<u8> = fs::read(path).expect("Couldn't read file");
+    // run(String::from_utf8(bytes).expect("Couldn't generate source string from bytes read"));
+
+    match fs::read_to_string(path) {
+        Ok(contents) => return run(&contents),
+        Err(msg) => return Err(msg.to_string()),
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() == 1 {
+        println!("Usage: jlox [script]");
+        process::exit(64);
+    } else if args.len() == 2 {
+        match run_file(&args[1]) {
+            Ok(_) => process::exit(0),
+            Err(msg) => {
+                println!("Error: {}", msg);
+                process::exit(1);
+            }
+        }
+    }
+}
