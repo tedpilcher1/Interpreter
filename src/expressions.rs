1c89ffc176d990ff9a052b88677bba11763e2d12
Ted Pilcher <ted.pilcher@moodys.com>
2024-08-19 09:12:53 +0100

    Commenting out printer for now as not required

--- src/expressions.rs
+++ src/expressions.rs
@@ -7,6 +7,22 @@ pub enum Expr {
     Grouping(GroupingExpression),
 }
 
+// impl ToString for Expr {
+//     fn to_string(&self) -> String {
+//         match self {
+//             Expr::Literal(literal) => match literal {
+//                 LiteralExpression::Number(num) => num.to_string(),
+//                 LiteralExpression::String(string) => string.to_string(),
+//                 LiteralExpression::Boolean(boolean) => boolean.to_string(),
+//                 LiteralExpression::Nil => "nil".to_string(),
+//             },
+//             Expr::Unary(unary) => unary.operator.to_string(),
+//             Expr::Binary(binary) => binary.operator.to_string(),
+//             Expr::Grouping(grouping) => grouping.to_string(),
+//         }
+//     }
+// }
+
 pub struct BinaryExpression {
     left: Box<Expr>,
     operator: TokenType,
--- src/printer.rs
+++ src/printer.rs
@@ -1,26 +1,28 @@
-use crate::expressions::{BinaryExpression, Expr};
+// use crate::expressions::{BinaryExpression, Expr};
 
-pub fn visit_binary_expr(expr: BinaryExpression) -> String {
-    todo!()
-}
-
-// fn parenthesize(string: &str) -> String {
-
-//     let mut parenthesized_string: String = "(".to_string();
-
-//     parenthesized_string.push_str(string);
-//     parenthesized_string.push_str(")");
-
-//     parenthesized_string
+// pub fn visit_binary_expr(expr: BinaryExpression) -> String {
+//     todo!()
 // }
 
-fn parenthesize(name: String, exprs: Vec<Expr>) -> String {
-    let mut output: String = "(".to_string();
+// // fn parenthesize(string: &str) -> String {
 
-    for expr in exprs {
-        output.push_str(" ");
-        output.push_str(expr)
-    }
+// //     let mut parenthesized_string: String = "(".to_string();
 
-    todo!()
-}
+// //     parenthesized_string.push_str(string);
+// //     parenthesized_string.push_str(")");
+
+// //     parenthesized_string
+// // }
+
+// fn parenthesize(name: String, exprs: Vec<Expr>) -> String {
+//     let mut output: String = "(".to_string();
+
+//     for expr in exprs {
+//         output.push_str(" ");
+//         output.push_str(expr)
+//     }
+
+//     output.push_str(")");
+
+//     todo!()
+// }
